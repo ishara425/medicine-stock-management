713c3dfdfdc306b38bfbac0f89e9dc545c031199
Ishara Palangasinghe <145181123+ishara425@users.noreply.github.com>
2025-11-10 12:47:06 +0530

    update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 // backend/controllers/authController.js
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // POST /api/auth/register (optional)
